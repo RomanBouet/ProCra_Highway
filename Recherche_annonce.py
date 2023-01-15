4e9e24567b1b1fd0c7aa8ef8eaad859bb57a4365
RomanBouet <bouetroman.pro@gmail.com>
2023-01-15 15:36:13 +0100

    ajout des url dans un dossier

--- Recherche_annonce.py
+++ Recherche_annonce.py
@@ -12,6 +12,7 @@ import feedparser
 import pickle
 from datetime import datetime
 import pandas as pd
+import configparser
 
 class Annonce(object):
     
@@ -43,33 +44,27 @@ class Catalogue(object):
 
 
 
-def Recherche_annonce(rss_url, contrat_rss, search_key, contrats):
+def Recherche_annonce(rss_url, contrat_rss, search_key, contrats, precision):
     # Parse le flux RSS à l'aide de feedparser
     feed = feedparser.parse(rss_url)
     annonce_contrat = []
+    print(rss_url)
 
-    #Boucle sur les mots contrats
-    for contrat in contrats:
-    # Boucle sur les entrées du flux
-        for entry in feed.entries:
-            if len(entry.tags[contrat_rss].term) != contrat:
-                annonce_contrat.append(Annonce(entry.title, entry.description, entry.link, entry.published, entry.tags[contrat_rss]))
-    
     annonce_keyword = []
-    #Boucle sur les mots clés
-    for elem in search_key:
-    # Boucle sur les entrées du flux
-        for entry in feed.entries:
-            if entry.tags[contrat_rss].term in contrats:
-                # Vérifie si le titre de l'entrée contient le mot-clé
-                similar_words = difflib.get_close_matches(elem, entry.description.split(), cutoff=0.7)
-                if len(similar_words)!=0:
-                    annonce_keyword.append(Annonce(entry.title, entry.description, entry.link, entry.published, entry.tags[contrat_rss].term))
-        
-            
+    annonce_parcouru = 0
 
-    a = len(feed.entries)
-    
+    for entry in feed.entries:
+        annonce_parcouru +=1
+        print(annonce_parcouru)
+        if entry.tags[contrat_rss].term in contrats:
+            for elem in search_key:
+                similar_words = difflib.get_close_matches(elem, entry.description.split(), cutoff=precision)
+                if len(similar_words) != 0:
+                    annonce_keyword.append(Annonce(entry.title, entry.description, entry.link, entry.published, entry.tags[contrat_rss].term))
+
+
+    test = feed.entries[1].title
+    print('annonce parcouru infunction : ',annonce_parcouru)        
     
     liste_offre = []
     for elem in annonce_keyword:
@@ -85,15 +80,15 @@ def Recherche_annonce(rss_url, contrat_rss, search_key, contrats):
                 Catalogue_offre.append(Annonce(entry.title, entry.description, entry.link, publiched_date, entry.tags[contrat_rss].term))
                 
         
-    print("nombre d'annonces parcourues : ",a)
+    #print("nombre d'annonces parcourues : ",annonce_parcouru)
     print("nombre d'annonces correctes : ",len(Catalogue_offre))
-    print("Les mots cles sont : ",search_key)
+    #print("Les mots cles sont : ",search_key)
     
     #sauvegarder le Catalogue dans un fichier pkl
     with open("Catalogue_offre_a_suppr.pkl", "wb") as f:
         pickle.dump(Catalogue_offre, f)
     
-    return Catalogue_offre
+    return Catalogue_offre, annonce_parcouru, test
 
 
 
@@ -103,17 +98,19 @@ def Recherche_annonce(rss_url, contrat_rss, search_key, contrats):
 
 
 # URL du flux RSS à analyser
-rss_url = ["https://brgm-recrute.talent-soft.com/handlers/offerRss.ashx?LCID=1036","https://www.emploi.cea.fr/handlers/offerRss.ashx?LCID=1036"]
-
+config_files_url = pd.read_csv('site_web.txt',header=0)
 # Recherche de mot clé
+
+
+#CONFIGPARSER FILE A AJOUTER
 search_key = ['géophysique',
               'traitement du signal',
-              'géologie',
+              'géologie'
     ]
 
+precision = 0.8
 
-contrat_rss = 1
-contrats = ['CDD','CDI']
+contrats = ['CDD','CDI','VIE']
 
 #########################################################
 #                       Main                            #
@@ -121,9 +118,13 @@ contrats = ['CDD','CDI']
 
 
 Offre_total = []
-for elem in rss_url:
-    Offres_corp = Recherche_annonce(elem, contrat_rss, search_key, contrats)
-    Offre_total = Offre_total+Offres_corp #  .append(Offres_corp)
+for elem in config_files_url.itertuples():
+    Offres_corp, annonce_parcouru,test = Recherche_annonce(elem[1], elem[2], search_key, contrats, precision)
+    Offre_total = Offre_total+Offres_corp
+    print("annonce parcouru : ",annonce_parcouru)
 
 df = pd.DataFrame([Offre.__dict__ for Offre in Offre_total])
 df.sort_values(by='published')
+
+
+df
